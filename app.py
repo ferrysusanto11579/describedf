cf3bbeed0afd0ae45126f05cd7334462c88cda50
ferrysusanto11579 <74810376+ferrysusanto11579@users.noreply.github.com>
2022-07-20 23:06:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -235,27 +235,19 @@ st.write('# Describe data frame')
 
 st.write('## Introduction')
 
-if st.checkbox('Objective'):
+if st.checkbox('Summary'):
 	st.write('''
-			* Perform exploratory data analysis using [Jupyter notebook](https://jupyter.org/)
-			* Data cleaning, feature engineering, and modelling
-			* Build an app using [Streamlit](https://docs.streamlit.io/en/stable/)
-			* Allow user to do exploratory analysis of a pre-selected suburb
-			* Using the pre-trained model, allow user to estimate/forecast Housing price
+			* Data source: [Adult Data Set @ UCI](https://archive.ics.uci.edu/ml/datasets/Adult)
 		''')
 
-if st.checkbox('Technical overview'):
+if st.checkbox('How to use this app'):
 	st.write('''
-			* Data source: [Melbourne Housing Market](https://www.kaggle.com/anthonypino/melbourne-housing-market)
-			* Exploratory data analysis notebook: [link](https://github.com/ferrysusanto11579/melb-housing/blob/main/notebook/Melbourne%20Housing%20Market%20-%20EDA.ipynb)
-			* Import raw data
-			* Data cleaning, missing data imputations, feature engineering
-			* ML modelling (xgboost)
-			* Model analysis (feature importance, explain the model using [SHAP](https://shap.readthedocs.io/en/latest/))
-			* Save the data & trained model (used as an input to this app)
-			* Build interactive visualisations (using altair package) to allow exploratory data analysis for a particular Suburb
-			* Predict the House Price using the ML model
-			* Display output for analysis
+			1. On the sidebar:
+				* Upload data (in csv format) or tick the box to load the sample dataset
+				* Configure the output file name
+			2. Preview the data
+			3. Investigate data statistics, including: count, null%, number of unique values, min, max, etc
+			4. Download the described data frame to your local to: easier investigation and note taking, etc
 		''')
 
 
