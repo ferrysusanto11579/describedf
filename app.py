dac9eda63cbd94af633560e45acdfe501b8a808a
ferrysusanto11579 <74810376+ferrysusanto11579@users.noreply.github.com>
2022-07-24 16:02:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -247,16 +247,6 @@ if st.checkbox('Summary'):
 				4. Download the _described data frame_ to your local to: easier investigation and note-taking, etc
 		''')
 
-if st.checkbox('How to use this app'):
-	st.write('''
-			1. On the sidebar:
-				* Upload data (in csv format) or tick the box to load the sample dataset
-				* Configure the output file name
-			2. Preview the data
-			3. Investigate data statistics, including: count, null%, number of unique values, min, max, etc
-			4. Download the _described data frame_ to your local to: easier investigation and note-taking, etc
-		''')
-
 
 if use_example_file:
 	uploaded_file = "adult.csv"
